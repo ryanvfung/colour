353083da9ea7bd6754b55132b3afdb9fe73c061d
Ryan Fung <arxqykhz@gmail.com>
2013-12-16 18:44:06 +0000

    0.4 Added I/O support for HSL, HSV, HSI

--- README.md
+++ README.md
@@ -10,11 +10,15 @@ A simple class-based method of manipulating colours by themselves, in terms of t
 
 **Created**: 2013-12-10
 
-**Last Modified**: 2013-12-15
+**Last Modified**: 2013-12-16
 
 
-Functions in the Colour module
+Functions in the colour module
 ------------------------------
+* **hsl(hue, sat, l)** -- Create and return a colour object based on HSL values, where 'hue' is in degrees, and both saturation 'sat' and lightness 'l' are out of 1
+* **hsv(hue, sat, v)** -- Create and return a colour object based on HSV values, where 'hue' is in degrees, and both saturation 'sat' and value 'v' are out of 1
+* **hsi(hue, sat, i)** -- Create and return a colour object based on HSI values, where 'hue' is in degrees, and both saturation 'sat' and intensity 'i' are out of 1
+* **hslv(hue, sat, arg, argtype)** -- Create and return a colour object based on HSL/HSV/HSI values, where 'hue' is in degrees, saturation 'sat' is out of 1, 'arg' is either lightness out of 1 or value out of 1, 'argtype' is 1 for HSL and 2 for HSV
 * **average(*args)** -- Returns the unweighted average of colours
 * **weightedAverage(colours, weights)** -- Returns the weighted average of colours
 
@@ -39,12 +43,39 @@ Data in the Colour class
 * **red** -- Red colour component out of 255
 * **green** -- Green colour component out of 255
 * **blue** -- Blue colour component out of 255
-* **rgb** -- Tuple (red,green,blue) of the RGB values
-* **hex** - 6-digit Hexadecimal representation in the form RRGGBB
+* **rgb** -- Tuple (red, green, blue) of the RGB values
+* **hex** -- 6-digit Hexadecimal representation in the form RRGGBB
+* **hsl** -- Tuple (hue, saturation, lightness)
+* **hsv** -- Tuple (hue, saturation, value)
+* **hsi** -- Tuple (hue, saturation, intensity)
+> For HSL, HSV and HSI tuple implementation:
+> * Hues are always in degrees
+> * Saturation, Lightness, Value and Intensity are all out of 1
+
+Data in the colour module
+-------------------------
+### HTML/CSS basic colour keywrds
+* **black** = Colour(0)
+* **silver** = Colour("C0C0C0")
+* **gray** = Colour("808080")
+* **white** = Colour(1)
+* **maroon** = Colour("800000")
+* **red** = Colour("F00")
+* **purple** = Colour("800080")
+* **fuchsia** = Colour("F0F")
+* **green** = Colour("008000")
+* **lime** = Colour("0F0")
+* **olive** = Colour("808000")
+* **yellow** = Colour("FF0")
+* **navy** = Colour("000080")
+* **blue** = Colour("00F")
+* **teal** = Colour("008080")
+* **aqua** = Colour("0FF")
+
 
 Upcoming Features to be Implemented
 -----------------------------------
-None
+* Generic expansion of features
 
 
 Change log
@@ -58,9 +89,14 @@ Change log
 * Changed truncation to rounding when converting from RGB to hex after initialising Colour object
 * Defined **trans**, **lighten**, **darken**, **complement** methods
 * Defined **rgb** value
-
-#### 2013-12-13 Version 0.2.1
-* Added support for tuple or list input arguments
+	#### 2013-12-13 Version 0.2.1
+	* Added support for tuple or list input arguments
 
 ### 2013-12-15 Version 0.3
 * Defined **average**, **weightedAverage** functions
+	#### 2013-12-15 Version 0.3.1
+	* Defined HTML/CSS basic colour keywrds
+
+### 2013-12-16 Version 0.4
+* Defined **hsl**, **hsv**, **hsi** functions to define Colour objects in those forms
+* Defined **hsl**, **hsv**, **hsi** values
--- archive/colour-0.3.py
+++ archive/colour-0.3.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """ RGB/Hex Colour Module
-    Version 0.4 release
+    Version 0.3 release
     @author: Ryan Fung
     Create Date: 2013-12-10
     Last Modified: 2013-12-13
@@ -15,48 +15,10 @@ class Colour(object):
         hexadecimal string; or a 6 digit hexadecimal string."""
         def colourConvert():
             """Post-process parsed red, green and blue values into hex"""
-            r = self.red
-            g = self.green
-            b = self.blue
-            self.hex = format(int(round(r)), "02X") + \
-                format(int(round(g)), "02X") + \
-                format(int(round(b)), "02X")
-            self.rgb = (r, g, b)
-            hueConvert()
-
-        def hueConvert():
-            """Calculates HSL, HSV, HSI values"""
-            r = self.red / 255.
-            g = self.green / 255.
-            b = self.blue / 255.
-            M = max(r, g, b)
-            m = min(r, g, b)
-            C = float(M - m)
-            if C == 0:
-                hue = 0
-            elif M == r:
-                hue = ((g-b)/C) % 6
-            elif M == g:
-                hue = ((b-r)/C) + 2
-            elif M == b:
-                hue = ((r-g)/C) + 4
-            hue *= 60  # normalising against 360°
-            self.hue = hue
-            # calculate lightness values
-            L = (M + m)/2.
-            V = M
-            I = (r + g + b)/3.
-            # calculate saturation values
-            if C == 0:
-                S_HSL = S_HSV = S_HSI = 0
-            else:
-                S_HSL = C/(1-abs((2*L)-1))
-                S_HSV = C/V
-                S_HSI = 1 - (float(m)/I)
-            self.hsl = (hue, S_HSL, L)
-            self.hsv = (hue, S_HSV, V)
-            self.hsi = (hue, S_HSI, I)
-
+            self.hex = format(int(round(self.red)), "02X") + \
+                format(int(round(self.green)), "02X") + \
+                format(int(round(self.blue)), "02X")
+            self.rgb = (self.red, self.green, self.blue)
         if len(args) == 1 and type(args[0]) in [tuple, list]:
             args = args[0]
         if len(args) == 3:
@@ -104,50 +66,31 @@ class Colour(object):
 of 255, a greyscale value out of either 1 or 255, or a hexadecimal string")
 
     def __str__(self):
-        """x.__str__() <==> str(x)"""
         return "rgba" + self.__repr__()[6:]
 
     def __repr__(self):
-        """x.__repr__() <==> repr(x)"""
         return "Colour({},{},{})".format(self.red, self.green, self.blue)
 
     def __gt__(self, other):
-        """x.__gt__(y) <==> x>y"""
         raise TypeError("no ordering relation is defined for colours")
 
     def __lt__(self, other):
-        """x.__lt__(y) <==> x<y"""
         raise TypeError("no ordering relation is defined for colours")
 
     def __ge__(self, other):
-        """x.__ge__(y) <==> x>=y"""
         raise TypeError("no ordering relation is defined for colours")
 
     def __le__(self, other):
-        """x.__le__(y) <==> x<=y"""
         raise TypeError("no ordering relation is defined for colours")
 
     def __eq__(self, other):
-        """x.__eq__(y) <==> x==y"""
-        return True if self.rgb == other.rgb else False
+        if self.rgb == other.rgb:
+            return True
+        else:
+            return False
 
     def __ne__(self, other):
-        """x.__ne__(y) <==> x!=y"""
         return not(self == other)
-
-    def __add__(self, other):
-        """x.__add__(y) <==> x+y
-        Add the RGB components up to a maximum of 255"""
-        red = self.red + other.red
-        green = self.green + other.green
-        blue = self.blue + other.blue
-        if red > 255:
-            red = 255
-        if green > 255:
-            green = 255
-        if blue > 255:
-            blue = 255
-        return Colour(red, green, blue)
 
     def trans(self, t, other):
         """Returns a Colour object representing the colour when the calling
@@ -177,84 +120,6 @@ of 255, a greyscale value out of either 1 or 255, or a hexadecimal string")
     def complement(self):
         """Returns the complement of the calling colour as a Colour object"""
         return Colour(255-self.red, 255-self.green, 255-self.blue)
-
-
-def hsl(hue, sat, l):
-    """Create and return a colour object based on HSL values, where 'hue' is
-    in degrees, and both saturation 'sat' and lightness 'l' are out of 1"""
-    if l < 0 or l > 1:
-        raise TypeError("Lightness must be between 0 and 1")
-    return hslv(hue, sat, l, 1)
-
-
-def hsv(hue, sat, v):
-    """Create and return a colour object based on HSV values, where 'hue' is
-    in degrees, and both saturation 'sat' and value 'v' are out of 1"""
-    if v < 0 or v > 1:
-        raise TypeError("Value must be between 0 and 1")
-    return hslv(hue, sat, v, 2)
-
-
-def hsi(hue, sat, i):
-    """Create and return a colour object based on HSI values, where 'hue' is
-    in degrees, and both saturation 'sat' and intensity 'i' are out of 1"""
-    if sat < 0 or sat > 1:
-        raise TypeError("Saturation must be between 0 and 1")
-    if i < 0 or i > 1:
-        raise TypeError("Intensity must be between 0 and 1")
-    from math import pi, cos
-    h = (hue % 120) * pi/180
-    x = i * (1 - sat)
-    y = i * (1 + float(sat*cos(h))/(cos((pi/3)-h)))
-    z = (3 * i) - (x + y)
-    if hue < 120:
-        rgb = (y, z, x)
-    elif hue < 240:
-        rgb = (x, y, z)
-    elif hue < 360:
-        rgb = (z, x, y)
-    else:
-        rgb = (0, 0, 0)
-    if max(rgb) > 1:
-        raise TypeError("Invalid HSI values - resultant RGB values outside \
-possible range")
-    return Colour(255 * rgb[0], 255 * rgb[1], 255 * rgb[2])
-
-
-def hslv(hue, sat, arg, argtype):
-    """Create and return a colour object based on HSL/HSV/HSI values, where
-    'hue' is in degrees, saturation 'sat' is out of 1, 'arg' is either
-    lightness out of 1 or value out of 1, 'argtype' is 1 for HSL
-    and 2 for HSV"""
-    if sat < 0 or sat > 1:
-        raise TypeError("Saturation must be between 0 and 1")
-    if argtype == 3:
-        raise NotImplementedError
-    if argtype == 1:
-        C = (1-abs((2*arg)-1)) * sat
-    elif argtype == 2:
-        C = arg * sat
-    H = hue / 60.
-    X = C * (1 - abs((H % 2) - 1))
-    if H < 1:
-        rgb = (C, X, 0)
-    elif H < 2:
-        rgb = (X, C, 0)
-    elif H < 3:
-        rgb = (0, C, X)
-    elif H < 4:
-        rgb = (0, X, C)
-    elif H < 5:
-        rgb = (X, 0, C)
-    elif H < 6:
-        rgb = (C, 0, X)
-    else:
-        rgb = (0, 0, 0)
-    if argtype == 1:
-        m = arg - (.5*C)
-    elif argtype == 2:
-        m = arg - C
-    return Colour(255*(rgb[0] + m), 255*(rgb[1] + m), 255*(rgb[2] + m))
 
 
 def average(*args):
@@ -289,21 +154,4 @@ same length")
         raise TypeError("Colours list must only contain colours, Weights \
 list must only contain numbers")
     return Colour(red, green, blue)
-
-#  define HTML/CSS basic colour keywrds
-black = Colour(0)
-silver = Colour("C0C0C0")
-gray = Colour("808080")
-white = Colour(1)
-maroon = Colour("800000")
-red = Colour("F00")
-purple = Colour("800080")
-fuchsia = Colour("F0F")
-green = Colour("008000")
-lime = Colour("0F0")
-olive = Colour("808000")
-yellow = Colour("FF0")
-navy = Colour("000080")
-blue = Colour("00F")
-teal = Colour("008080")
-aqua = Colour("0FF")
+    
